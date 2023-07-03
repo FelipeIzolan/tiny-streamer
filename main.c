17d60624f4c4151dbc97b8c9b82f5dff0c046947
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2023-07-03 02:17:43 -0300

    update

--- main.c
+++ main.c
@@ -4,24 +4,45 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
 
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-extern int array_has(int arr[], int value);
 extern int update_history_index();
+extern int array_int_has(int arr[], int value);
+extern int get_length_in_directory(const char *p);
+extern void read_files_in_directory(char ** addr, const char *p);
 
 int is_speaking = 0;
-int is_speaking_history[8] = {0,0,0,0,0,0,0,0};
+int is_speaking_history[4] = {};
+
 
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
 {
-  is_speaking_history[update_history_index()] = *((float*)pInput) <= -0.01 ? 1 : 0;
-  is_speaking=array_has(is_speaking_history,1);
-  // is_speaking = *((float*)pInput) <= -0.01 ? 1 : 0;
-
+  is_speaking_history[update_history_index()] = *((float*)pInput) <= -0.009 ? 1 : 0;
+  is_speaking=array_int_has(is_speaking_history,1);
 }
 
 int main(void) {
+  // ----- assets
+  int sprite_idle_frames = get_length_in_directory(".//assets//idle");
+  double sprite_idle_current_frame = 0;
+  
+  char *sprite_idle_file[sprite_idle_frames];
+  kit_Image *sprite_idle_kit[sprite_idle_frames];
+
+  read_files_in_directory(sprite_idle_file, ".//assets//idle");
+
+  for (int i = 0; i<sprite_idle_frames; i++) {
+    char src[] = ".//assets//idle//";
+    strcat(src,sprite_idle_file[i]);
+
+    sprite_idle_kit[i] = kit_load_image_file(src);
+  }
+  // ------
+
+  // ------ voice detector
   ma_device device;
 
   ma_device_config config  = ma_device_config_init(ma_device_type_capture);
@@ -32,16 +53,41 @@ int main(void) {
 
   if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) return -1;
   ma_device_start(&device);
+  // ------
 
-  kit_Context *ctx = kit_create("Tiny Streamer", 128, 128, KIT_SCALE2X, "icon.ico");
+  // ------ ui
+  kit_Context *ctx = kit_create(
+      "Tiny Streamer",
+      sprite_idle_kit[0]->w * 4,
+      sprite_idle_kit[0]->h * 4,
+      KIT_SCALE2X, 
+      "icon.ico"
+  );
+
+  printf("%i", ctx->win_w);
+
   double delta;
+  int x = (ctx->win_w/2-sprite_idle_kit[0]->w)/2;
+  int y = (ctx->win_h/2-sprite_idle_kit[0]->h)/2;
 
   while (kit_step(ctx, &delta)) {
       if (kit_key_pressed(ctx, VK_ESCAPE)) { break; }
-      kit_draw_rect(ctx, is_speaking ? kit_rgb(0,255,0) : kit_rgb(255,0,0),KIT_BIG_RECT);
+
+      kit_clear(ctx, KIT_BLACK);
+      
+      if (!is_speaking) {
+        sprite_idle_current_frame += delta / 0.2;
+        int frame = (int) abs(sprite_idle_current_frame);
+        if (frame>=sprite_idle_frames-1) sprite_idle_current_frame = 0;
+        kit_draw_image(ctx, sprite_idle_kit[frame], x, y);
+      } else {
+        // continue....
+      }
   }
+  // ------
 
   kit_destroy(ctx);
   ma_device_uninit(&device);
+
   return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,8 @@
-// check if int array have a specific int value.
-int array_has(int arr[], int value) {
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+
+int array_int_has(int arr[], int value) {
   for (int i=0; i < sizeof(*arr); i++) {
     if (arr[i] == value) return 1;
   }
@@ -10,6 +13,39 @@ int array_has(int arr[], int value) {
 int update_history_index() {
   static int x = -1;
   x++;
-  if (x > 7) x=-1;
+  if (x > 3) x=-1;
   return x;
+}
+
+int get_length_in_directory(const char *p) {
+  DIR *x = opendir(p);  
+  struct dirent *f;
+  int count = 0;
+
+  while((f = readdir(x)) != NULL) {
+    if (strcmp(f->d_name, ".") && strcmp(f->d_name, "..")) {
+      count++;
+    }
+  }
+
+  closedir(x);
+
+  return count;
+}
+
+void read_files_in_directory(char ** addr, const char *p) {
+  DIR *x = opendir(p);
+  struct dirent *f;
+  int i = 0;
+
+  while ((f=readdir(x)) != NULL) {
+    if (strcmp(f->d_name, ".") && strcmp(f->d_name, "..")) {
+      char *filename = malloc(256);
+      strcpy(filename,f->d_name);
+      addr[i] = filename;
+      i++;
+    }
+  }
+
+  closedir(x);
 }
